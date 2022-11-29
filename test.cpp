6b821f993bcdbb144718e002c6bbfcf55220c5d7
gitBin <2782694792@qq.com>
2022-11-29 16:37:47 +0800

    添加测试 log4cplus 的使用

--- test.cpp
+++ test.cpp
@@ -1,10 +1,16 @@
 #include "XmlParser.h"
 
 #include <iostream>
+#include <string>
 
+#include <log4cplus/initializer.h>
+#include <log4cplus/consoleappender.h>
+#include <log4cplus/fileappender.h>
+#include <log4cplus/logger.h>
+#include <log4cplus/loggingmacros.h>
 
-int main( )
-{
+auto fun = [ ]( ) {
+
 	Xml x;
 	x.setName( "XML" );
 	x.setAttr( "ID", 1 );
@@ -33,6 +39,40 @@ int main( )
 	x1.append( x3 );
 	x.append( x1 );
 
+	string opt = x.serial( );
+	return opt;
+};
+
+#define LOGPROPERTY "logconfig.property"
+
+int main( )
+{
+	//! 1、初始化 initialize
+	log4cplus::initialize( );
+	//! 2、输出介质实例化 appender
+	log4cplus::SharedAppenderPtr  _consoleAppender( new log4cplus::ConsoleAppender );
+	log4cplus::SharedAppenderPtr _fileAppender( new log4cplus::FileAppender(
+		LOG4CPLUS_TEXT( "test.log" ), std::ios_base::app & std::ios_base::ate ) );
+	//! 3、输出格式实例化 layout
+	log4cplus::SimpleLayout * _consoleLayout = new log4cplus::SimpleLayout( );
+	log4cplus::tstring pattern = LOG4CPLUS_TEXT( "[%D{%Y-%m-%d %H:%M:%S,%Q}] [%t] %-5p - %m%n[%p]" );
+	log4cplus::PatternLayout * _patternLayout = new log4cplus::PatternLayout( pattern );
+	//! 4、layout 输出介质对象绑定到 appender 输出格式对象
+	//!? 省略 layout 输出介质对象绑定到 appender 输出格式对象，简单布局器会绑定到 logger 日志输出对象
+	//_consoleAppender->setLayout( std::unique_ptr<log4cplus::Layout>( _consoleLayout ) );
+	//_fileAppender->setLayout( std::unique_ptr<log4cplus::Layout>( _patternLayout ) );
+	_consoleAppender->setLayout( std::unique_ptr<log4cplus::Layout>( _patternLayout ) );
+	//! 5、日志输出实例化 logger
+	log4cplus::Logger logger = log4cplus::Logger::getInstance( LOG4CPLUS_TEXT( "test" ) );
+	//! 6、日志等级、输出介质绑定
+	logger.setLogLevel( log4cplus::DEBUG_LOG_LEVEL );
+	logger.addAppender( _consoleAppender );
+	logger.addAppender( _fileAppender );
+
+	LOG4CPLUS_DEBUG( logger, "test : " << fun( ).c_str( ) );
+
+	//cout << opt << endl;
+
 	// 遍历所有节点
 	//cout << opt << endl;
 
@@ -55,26 +95,24 @@ int main( )
 	// 删除指定节点下的对应节点
 	//x1.erase( x1.begin() );
 
-	//string opt = x.serial( );
-	//cout << opt << endl;
-
 	// 获取解析后的 xml
-	Xml pom;
+	//Xml pom;
+
 	/*if ( !pom.load( "pomTest.xml" ) )
 	{
 		cout << "not found the file" << endl;
 	}*/
 
-	ifstream fin( "pomTest.xml" );
-	if ( !pom.load( fin ) )
-	{
-		cout << "not found the file" << endl;
-	}
+	//ifstream fin( "pomTest.xml" );
+	//if ( !pom.load( fin ) )
+	//{
+	//	cout << "not found the file" << endl;
+	//}
 
 	//const string pomStr = pom.serial( );
 
-	const string pomStr = pom["description" ].serial( );
-	cout << "project : description = \n\t\t" << pomStr << endl;
+	//const string pomStr = pom["description" ].serial( );
+	//cout << "project : description = \n\t\t" << pomStr << endl;
 
 	//pom.save("pomTest111.xml" );
 
